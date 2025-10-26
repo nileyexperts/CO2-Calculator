b352c2f33c41ae338e32018451b583bbc31506be
nileyexperts <niley.experts@gmail.com>
2025-10-26 10:24:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1415,4 +1415,3 @@ if st.button("Calculer l'empreinte carbone totale", disabled=not can_calculate):
                 import traceback; st.code(traceback.format_exc())
     else:
         st.info("Aucun segment valide n'a été calculé. Vérifiez les entrées ou les sélections.")
-``
